b5d62b25538e77620f8afea02e78ba2a3b24d7df
Raphael Marques <raphaeljcmarques@gmail.com>
2024-11-30 18:46:54 -0300

    feat: get user metrics endpoint

--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -5,7 +5,7 @@ declare module 'knex/types/tables' {
   export interface Tables {
     meals: {
       id: string;
-      user_id: string;
+      user_id?: string;
       name: string;
       description?: string;
       followed_diet: boolean;
--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -29,16 +29,98 @@ export async function mealsRoutes(app: FastifyInstance) {
       const { mealId } = getMealParams.parse(req.params);
       const { userId } = req.cookies;
 
-      const meal = await knex('meals').select().where({
-        user_id: userId,
-        id: mealId,
-      });
+      const meal = await knex('meals')
+        .select()
+        .where({
+          user_id: userId,
+          id: mealId,
+        })
+        .first();
 
       return reply.send({
         meal,
       });
     },
   );
+
+  app.get(
+    '/metrics',
+    { preHandler: [checkUserIdIdentity] },
+    async (req, reply) => {
+      const { userId } = req.cookies;
+
+      const meals = await knex('meals')
+        .select()
+        .where({
+          user_id: userId,
+        })
+        .orderBy('meal_time', 'asc');
+
+      const totalUnfollowedMeals = meals.filter(
+        meal => !meal.followed_diet,
+      ).length;
+
+      const totalFollowedMeals = meals.filter(
+        meal => meal.followed_diet,
+      ).length;
+
+      const totalMeals = meals.length;
+
+      let bestSequence = 0;
+      let currentSequence = 0;
+
+      for (const meal of meals) {
+        if (meal.followed_diet) {
+          currentSequence++;
+          bestSequence = Math.max(bestSequence, currentSequence);
+        } else {
+          currentSequence = 0;
+        }
+      }
+
+      return reply.send({
+        metrics: {
+          totalMeals,
+          totalFollowedMeals,
+          totalUnfollowedMeals,
+          bestSequence,
+        },
+      });
+    },
+  );
+
+  app.post('/', async (req, reply) => {
+    const createMealSchema = z.object({
+      name: z.string(),
+      description: z.string().optional(),
+      mealTime: z.string(),
+      followedDiet: z.boolean(),
+    });
+
+    const { name, description, mealTime, followedDiet } =
+      createMealSchema.parse(req.body);
+
+    let userId = req.cookies.userId;
+
+    if (!userId) {
+      userId = randomUUID();
+      reply.cookie('userId', userId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7, // 7 days
+      });
+    }
+
+    await knex('meals').insert({
+      id: randomUUID(),
+      name,
+      description,
+      meal_time: mealTime,
+      followed_diet: followedDiet,
+      user_id: userId,
+    });
+
+    return reply.status(201).send();
+  });
 
   app.put(
     '/:mealId',
@@ -92,37 +174,4 @@ export async function mealsRoutes(app: FastifyInstance) {
       return reply.status(204).send();
     },
   );
-
-  app.post('/', async (req, reply) => {
-    const createMealSchema = z.object({
-      name: z.string(),
-      description: z.string().optional(),
-      mealTime: z.string(),
-      followedDiet: z.boolean(),
-    });
-
-    const { name, description, mealTime, followedDiet } =
-      createMealSchema.parse(req.body);
-
-    let userId = req.cookies.userId;
-
-    if (!userId) {
-      userId = randomUUID();
-      reply.cookie('userId', userId, {
-        path: '/',
-        maxAge: 60 * 60 * 24 * 7, // 7 days
-      });
-    }
-
-    await knex('meals').insert({
-      id: randomUUID(),
-      name,
-      description,
-      meal_time: mealTime,
-      followed_diet: followedDiet,
-      user_id: userId,
-    });
-
-    return reply.status(201).send();
-  });
 }
